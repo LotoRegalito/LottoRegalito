3681ff164046f60987ff6e33c7c7c9b5aa904f98
Glicht <rosalesvallorani@gmail.com>
2023-09-04 09:05:21 -0400

    [principal] - cambios

--- Resultados/index.php
+++ Resultados/index.php
@@ -153,7 +153,7 @@
                     </svg>
                 </label>
                 <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a href="../Afiche/index.html">Afiche</a></li>
+                    <li><a href="../Afiche/index.html">Afiche HD</a></li>
                     <li><a href="../#">Contactanos</a></li>
                 </ul>
             </div>
@@ -176,7 +176,7 @@
                     <path d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z" />
                 </svg>
 
-                Afiche</a>
+               Afiche HD</a>
             <a href="../#Contactanos" class="[ btn btn-primary text-white hidden ] [ md:flex ] [ lg:flex ]"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                 </svg>
