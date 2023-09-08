3fdcda3c8259bca0b663e2bdd36536d4e6449b63
Glicht <rosalesvallorani@gmail.com>
2023-09-08 15:16:45 -0400

    [modulos] - quitamos el loader

--- Resultados/index.php
+++ Resultados/index.php
@@ -78,7 +78,7 @@
 
         console.log('listo');
 
-        $("#loader").fadeOut("slow");
+      //  $("#loader").fadeOut("slow");
 
     });
 </script>
@@ -133,6 +133,7 @@
 
 
     <!-- Contenedor del loader -->
+    <!----
     <div id="loader" class="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-white   z-50">
         <div class="loader">
             <div class="lds-ring">
@@ -143,6 +144,7 @@
             </div>
         </div>
     </div>
+    --->
 
     <div class="navbar bg-base-100 z-20">
         <div class="navbar-start">
@@ -176,7 +178,7 @@
                     <path d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z" />
                 </svg>
 
-               Afiche HD</a>
+                Afiche HD</a>
             <a href="../#Contactanos" class="[ btn btn-primary text-white hidden ] [ md:flex ] [ lg:flex ]"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
                     <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
                 </svg>
